3736c2846427ce22b8acee85cf6b697eef508b80
itpaaradron <bankchuken@gmail.com>
2023-07-20 11:07:03 +0700

    update

--- lib/screen/add_water_manu.dart
+++ lib/screen/add_water_manu.dart
@@ -1,5 +1,13 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:application_drinking_water_shop/utility/my_constant.dart';
 import 'package:application_drinking_water_shop/utility/my_style.dart';
+import 'package:application_drinking_water_shop/utility/normal_dialog.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AddMenuWater extends StatefulWidget {
   const AddMenuWater({super.key});
@@ -9,6 +17,9 @@ class AddMenuWater extends StatefulWidget {
 }
 
 class _AddMenuWaterState extends State<AddMenuWater> {
+  File? file;
+  String? nameWhat, price, detail;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,13 +50,65 @@ class _AddMenuWaterState extends State<AddMenuWater> {
       margin: EdgeInsetsDirectional.only(top: 2.0),
       width: 300.0,
       child: ElevatedButton.icon(
-          onPressed: () {}, icon: Icon(Icons.save_alt), label: Text('บันทึก')),
+        onPressed: () {
+          if (file == null) {
+            normalDialog(context, 'กรุณาเลือกรูปภาพ !');
+          } else if (nameWhat == null ||
+              nameWhat!.isEmpty ||
+              price == null ||
+              price!.isEmpty ||
+              detail == null ||
+              detail!.isEmpty) {
+            normalDialog(context, 'กรุณากรอกข้อมูลทุกช่อง !');
+          } else {
+            uploadWaterAndInsertData();
+          }
+        },
+        icon: Icon(Icons.save_alt),
+        label: Text('บันทึก'),
+      ),
     );
   }
+
+  Future<Null> uploadWaterAndInsertData ()async{
+
+    String? urlUpload = '${MyConstant().domain}/WaterShop/savewater.php';
+
+    Random  random = Random();
+    int i = random.nextInt(1000000);
+    String? nameFile = 'what$i.jpg';
+
+    try {
+      
+       Map<String, dynamic> map = Map();
+       map['file'] = await MultipartFile.fromFile(file!.path, filename: nameFile);
+       FormData formData = FormData.fromMap(map);
+
+       await Dio().post(urlUpload,data: formData).then((value) async {
+        String? urlPathImage = '/WaterShop/water/$nameFile';
+
+        print('urlPathImage = ${MyConstant().domain}$urlPathImage');
+
+        SharedPreferences preferences = await SharedPreferences.getInstance();
+        String? idShop = preferences.getString('id');
+
+
+        String? urlInsertData = '${MyConstant().domain}/WaterShop/addWater.php?isAdd=true&idShop=$idShop&NameWater=$nameWhat&PathImage=$urlPathImage&Price=$price&Detail=$detail';
+        await Dio().get(urlInsertData).then((value) => Navigator.pop(context));
+
+       },);
+
+    } catch (e) {
+      
+    }
+
+  }
+
 
   Widget nameForm() => Container(
         width: 250.0,
         child: TextField(
+          onChanged: (value) => nameWhat = value.trim(),
           decoration: InputDecoration(
             prefixIcon: Icon(Icons.water_drop_outlined),
             labelText: 'ชื่อน้ำดื่ม',
@@ -56,7 +119,8 @@ class _AddMenuWaterState extends State<AddMenuWater> {
 
   Widget pricForm() => Container(
         width: 250.0,
-        child: TextField(
+        child: TextField(keyboardType: TextInputType.number,
+          onChanged: (value) => price = value.trim(),
           decoration: InputDecoration(
             prefixIcon: Icon(Icons.monetization_on_outlined),
             labelText: 'ราคาน้ำดื่ม',
@@ -68,6 +132,7 @@ class _AddMenuWaterState extends State<AddMenuWater> {
   Widget detaiForm() => Container(
         width: 250.0,
         child: TextField(
+          onChanged: (value) => detail = value.trim(),
           keyboardType: TextInputType.multiline,
           maxLines: 3,
           decoration: InputDecoration(
@@ -84,19 +149,34 @@ class _AddMenuWaterState extends State<AddMenuWater> {
       children: <Widget>[
         IconButton(
           icon: Icon(Icons.add_a_photo),
-          onPressed: null,
+          onPressed: () => chooseImage(ImageSource.camera),
         ),
         Container(
-          width: 250.0,
-          height: 250.0,
-          child: Image.asset('images/WaterMenu.png'),
+          width: 230.0,
+          height: 230.0,
+          child: file == null
+              ? Image.asset('images/WaterMenu.png')
+              : Image.file(file!),
         ),
         IconButton(
           icon: Icon(Icons.add_photo_alternate),
-          onPressed: null,
+          onPressed: () => chooseImage(ImageSource.gallery),
         ),
       ],
     );
+  }
+
+  Future<Null> chooseImage(ImageSource imageSource) async {
+    try {
+      var object = await ImagePicker().pickImage(
+        source: imageSource,
+        maxWidth: 800.0,
+        maxHeight: 800.0,
+      );
+      setState(() {
+        file = File(object!.path);
+      });
+    } catch (e) {}
   }
 
   Widget showTitleWater(String string) {
